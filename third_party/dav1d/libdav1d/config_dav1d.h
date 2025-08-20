d0e9259668cafabd2f1de0a9aeadb86a48ece517
coneco-cy <chengya@qti.qualcomm.com>
2025-08-20 17:14:53 +0800

    enable arm64 compilation(the progress is 60%).

--- third_party/dav1d/libdav1d/config_dav1d.h
+++ third_party/dav1d/libdav1d/config_dav1d.h
@@ -3,6 +3,8 @@
 
 #if defined(_M_IX86) || defined(__i386__) || defined(_M_X64) || defined(__x86_64__)
 #include "../config/win/x86/config_dav1d_x86.h"
+#else
+#include "../config/win/arm64/config.h"
 #endif
 
 #else
